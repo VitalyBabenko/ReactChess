73c96691010997b8d50e225b58d836420e475c8e
Vitaly <itassan6@gmail.com>
2023-02-14 11:22:00 +0200

    Added responsiveness in PlayerComponent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
